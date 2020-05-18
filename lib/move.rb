d11c37c5068478368acfe2ff7e0232ec35f5db71
Valentina E <tiyahelfrih@gmail.com>
2020-05-18 22:11:30 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -4,6 +4,7 @@ def display_board
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  binding.pry
   input.to_i.between?(1, 9)
 end
 
