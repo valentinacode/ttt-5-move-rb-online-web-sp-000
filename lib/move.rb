f25d0ce14f4a01e5d627418315f5f202a82db855
Valentina E <tiyahelfrih@gmail.com>
2020-05-18 22:16:05 +0000

    Automatically backed up by Learn

---
+++ lib/move.rb
@@ -0,0 +1,17 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  index = user_input.to_i - 1
+end
+
+def move(board, index, value="X")
+  board[index] = value
+end
